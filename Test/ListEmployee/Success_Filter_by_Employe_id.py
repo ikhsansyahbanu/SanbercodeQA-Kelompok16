814157aec133a5a47e430d47b9492d8bded70b8b
IKHSAN\ikhsan <syahbanu1997@gmail.com>
2023-02-26 20:22:15 +0700

    employee

---
+++ Test/ListEmployee/Success_Filter_by_Employe_id.py
@@ -0,0 +1,29 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from object.pageObject import PageObject
+
+class TestFilterByEmployeeId(unittest.TestCase):
+    # Test Case ID: TC05
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.maximize_window()
+        PageObject.login(self, self.browser)
+
+    def test_filter_by_employee_id (self):
+        driver = self.browser
+        PageObject.clickByXpath(self, "//*[@id='app']/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")
+        PageObject.clickByXpath(self, "//*[@id='app']/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a")
+        PageObject.inputByXpath(self, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input", "0054")
+        PageObject.clickByXpath(self, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]")
+        
+        # Validation 
+        title = driver.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[2]")
+        print(title)
+        self.assertIn('0054', title)
+
+    def tearDown(self):
+        self.browser.close()
+
+if __name__ == "__main__":
+    unittest.main()
